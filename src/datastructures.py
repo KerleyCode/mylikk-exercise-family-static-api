f495808ff233365897365cd957f9de6e49456b4a
My'lik Kerley <159506912+KerleyCode@users.noreply.github.com>
2024-07-22 01:37:11 +0000

    done

--- src/datastructures.py
+++ src/datastructures.py
@@ -1,36 +1,40 @@
-
-"""
-update this file to implement the following already declared methods:
-- add_member: Should add a member to the self._members list
-- delete_member: Should delete a member from the self._members list
-- update_member: Should update a member from the self._members list
-- get_member: Should return a member from the self._members list
-"""
 from random import randint
 
 class FamilyStructure:
     def __init__(self, last_name):
         self.last_name = last_name
-
-        # example list of members
+        self._next_id = 1
         self._members = []
+        self._initialize_members()
 
-    # read-only: Use this method to generate random members ID's when adding members into the list
-    def _generateId(self):
-        return randint(0, 99999999)
+    def _generate_id(self):
+        generated_id = self._next_id
+        self._next_id += 1
+        return generated_id
 
     def add_member(self, member):
-        # fill this method and update the return
-        pass
+        if 'id' not in member:
+            member['id'] = self._generate_id()
+        member['last_name'] = self.last_name
+        self._members.append(member)
 
     def delete_member(self, id):
-        # fill this method and update the return
-        pass
+        self._members = [member for member in self._members if member['id'] != id]
 
     def get_member(self, id):
-        # fill this method and update the return
-        pass
+        return next((member for member in self._members if member['id'] == id), None)
 
-    # this method is done, it returns a list with all the family members
     def get_all_members(self):
         return self._members
+
+    def _initialize_members(self):
+        initial_members = [
+            {"first_name": "John", "age": 33, "lucky_numbers": [7, 13, 22]},
+            {"first_name": "Jane", "age": 35, "lucky_numbers": [10, 14, 3]},
+            {"first_name": "Jimmy", "age": 5, "lucky_numbers": [1]}
+        ]
+        for member in initial_members:
+            self.add_member(member)
+
+# Initialize the Jackson family
+jackson_family = FamilyStructure('Jackson')
